554104aa4e6a704bcf534042d89d16fc9334b47e
aditya <aditya031997@gmail.com>
2021-10-04 11:24:43 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Json from "./Json";
+// import New from "./New";
+function App() {
+  return (
+    <div>
+      <Json />
+      {/* <New /> */}
+    </div>
+  );
+}
+
+export default App;
